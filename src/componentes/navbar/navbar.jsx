fd8e638468a447b87c55bf0331ad55c8c72082fe
FrankLDF <frankluisduarte00@gmail.com>
2024-07-23 09:15:59 -0400

    corrigiendo position del nav

--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -1,19 +1,35 @@
-import nav from './navbar.module.css'
-import LogoT from '../../assets/logo.png';
+import { useEffect } from "react";
+
+import nav from "./navbar.module.css";
+import LogoT from "../../assets/logo.png";
 
 export default function Navbar() {
-    return (
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector(`.${nav.header}`);
+      if (window.scrollY > 50) {
+        header.classList.add(nav.scrolled);
+      } else {
+        header.classList.remove(nav.scrolled);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
     <header className={nav.header}>
-        <img src={LogoT} className={nav.logo} alt='logo'/>
+      <img src={LogoT} className={nav.logo} alt="logo" />
 
-        <nav className={nav.navbar}>
-            <a href='/'>Inicio de seccion</a>
-            <a href='/'>Sobre nosotros</a>
-            <a href='/'>Nuestras redes</a>
-
-        </nav>
+      <nav className={nav.navbar}>
+        <a href="/">Inicio de seccion</a>
+        <a href="/">Sobre nosotros</a>
+        <a href="/">Nuestras redes</a>
+      </nav>
     </header>
-  )
+  );
 }
-
-
